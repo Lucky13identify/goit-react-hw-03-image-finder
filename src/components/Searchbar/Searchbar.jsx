25b7735270d6a5a5a83c0caaf6bce1fc5778bf9b
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-03-17 17:37:29 +0200

    new

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { AiFillBank } from 'react-icons/ai';
 import { Header, Form, Button } from './Serchbar.styled';
 
 export const Searchbar = ({ onSubmit, onChange }) => {
@@ -6,7 +7,9 @@ export const Searchbar = ({ onSubmit, onChange }) => {
     <Header className="searchbar">
       <Form className="form" onSubmit={onSubmit}>
         <Button type="submit" className="button">
-          <span className="button-label">Search</span>
+          <span className="button-label">
+            <AiFillBank />
+          </span>
         </Button>
 
         <input
